2cc3172fe1d47d3cdf038c6a21233dc23de84817
Richard Shea <rshea@thecubagroup.com>
2018-10-21 00:01:32 +1300

    After installing ember-web-app

---
+++ config/manifest.js
@@ -0,0 +1,22 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = function(/* environment, appConfig */) {
+  // See https://github.com/san650/ember-web-app#documentation for a list of
+  // supported properties
+
+  return {
+    name: "my-pwa",
+    short_name: "my-pwa",
+    description: "",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+    ],
+    ms: {
+      tileColor: '#fff'
+    }
+  };
+}
